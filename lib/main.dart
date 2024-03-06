b99a0fed73dc2575ff52cc0ab9ba6199ebd3ff32
Akash <AwesomeAkash47@gmail.com>
2024-03-06 23:19:51 +0530

    main build

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       home: SafeArea(child: Column(
         children: [
           Expanded(child: MyWebView()),
+          Text("cmon work"),
         ],
       )),
     );
